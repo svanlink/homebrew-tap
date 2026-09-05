1f190ccf9d30e94242482c6dd47af7734462fd3c
vaneickelen <vaneickelen.smo91@gmail.com>
2026-09-05 11:50:08 +0200

    cutaway 1.2.0

--- Casks/cutaway.rb
+++ Casks/cutaway.rb
@@ -1,6 +1,6 @@
 cask "cutaway" do
-  version "1.1.0"
-  sha256 "4988a85953a1b4dee97fe6cf664dbfe41cb8f56330eeb5eba270dfe453eed82c"
+  version "1.2.0"
+  sha256 "af3a9c13334a77bfa9f366f1566959c7e153bf73fdaabfe0ee492d0190c4cf9a"
 
   url "https://github.com/svanlink/cutaway/releases/download/v#{version}/Cutaway-#{version}.zip"
   name "Cutaway"
